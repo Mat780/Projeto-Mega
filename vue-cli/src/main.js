94dd393d98359bb1d4e91efbda6b446b6172d3c5
Mat780 <mateusfelipe10@live.com>
2021-09-29 21:16:38 -0400

    VOLTO

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
-import LandingPage from './LandingPage.vue'
+import Pagina from './Pagina.vue'
+import router from './router'
 
-createApp(LandingPage).mount('#app')
+createApp(Pagina).use(router).mount('#app')
+
+//createApp(LandingPage).mount('#app')
+//import LandingPage from './LandingPage.vue'
