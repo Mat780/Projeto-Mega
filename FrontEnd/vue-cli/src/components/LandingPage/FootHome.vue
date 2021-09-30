b2d0964f12d48864db8f2d8c6aafbb31ee615193
Raul <raulleaochagas@gmail.com>
2021-09-29 21:50:47 -0400

    commit

---
+++ FrontEnd/vue-cli/src/components/LandingPage/FootHome.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer id="Foot">
+        <img :src="pato" />
+
+        <span id="frase-rodapé">
+            <h2>Duck Health</h2>
+        </span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "FootHome",
+    data(){
+        return{
+            pato: '/img/DuckHealth_SVG.svg',
+        }
+    }
+}
+</script>
---
+++ FrontEnd/vue-cli/src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Aqui é a pagina do Login :V 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+    
+}
+</script>
